95cfa22b49d8e175315d625cca2fa1c221bfde05
root <root@ip-172-31-36-130.ap-south-1.compute.internal>
2023-08-08 11:51:15 +0000

    added the data

--- main.tf
+++ main.tf
@@ -47,4 +47,38 @@ resource "aws_instance" "eks-master1" {
  #  vpc_security_group_ids = sg-03cde4e562a7e79b3
  }
 
+# creating a internet gatewat
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "project IGW"
+  }
+  
+}
+
+# creating a 2nd Route table
+# We already know that when a VPC is created, a main route table is created as well. The main route table is responsible for enabling the flow of traffic within the VPC. 
+
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.main.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+ 
+ tags = {
+   Name = "2nd Route Table"
+ }
+}
+
+#  Associating Public Subnets to the Second Route Table
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.second_rt.id
+}
+
 
